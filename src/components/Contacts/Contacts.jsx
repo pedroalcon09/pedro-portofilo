0e92d1c45ddce321e580e4dc9e0944eb785e243e
pedroalcon09 <pedro09alcon@gmail.com>
2024-08-16 00:17:54 -0300

    fix footer

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -131,9 +131,9 @@ export const Contacts = () => {
               whileInView={{ opacity: 0 }}
               transition={{ delay: 3, duration: 1 }}
             >
-              <svg width="310px" height="310px" viewBox="0 0 31.666 32.666">
+              <svg width="310px" height="310px" viewBox="0 0 32.666 32.666">
                 <motion.path
-                  strokeWidth={0.2}
+                  strokeWidth={0.3}
                   fill="none"
                   initial={{ pathLength: 0 }}
                   transition={{ duration: 3 }}
